5b72057ea3b82354d06cb8e9186d76f2da121fb4
Julian Ng <julian.ng@softwire.com>
2018-06-12 17:13:28 +0100

    Implemented Scanner class for user input to define program max number

--- src/main/java/julesnuggy/fizzbuzz/Main.java
+++ src/main/java/julesnuggy/fizzbuzz/Main.java
@@ -1,10 +1,16 @@
 package julesnuggy.fizzbuzz;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         FizzBuzz fb = new FizzBuzz();
-        for (int i=1; i<=255; i++) {
+        Scanner userInput = new Scanner(System.in);
+        System.out.print("Enter the number to run FizzBuzz up to: ");
+        int maxNumber = userInput.nextInt();
+
+        for (int i=1; i<=maxNumber; i++) {
             System.out.println(fb.runFizzBuzz(i));
         }
     }
