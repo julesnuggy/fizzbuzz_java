c2a1e6993b881dac95631acc02db17c8b5da17af
Julian Ng <julian.ng@softwire.com>
2018-06-12 17:28:07 +0100

    GREEN: user can define which rules to use or not

--- src/main/java/julesnuggy/fizzbuzz/FizzBuzz.java
+++ src/main/java/julesnuggy/fizzbuzz/FizzBuzz.java
@@ -4,8 +4,8 @@ import java.util.Collections;
 import java.util.ArrayList;
 
 public class FizzBuzz {
-    public String assessNumber(int inputNumber, int assessNumber, String outputString) {
-        if (inputNumber % assessNumber == 0) {
+    public String assessNumber(int inputNumber, int assessNumber, String outputString, Boolean ruleActive) {
+        if (inputNumber % assessNumber == 0 && ruleActive == true) {
             return outputString;
         }
         else {
@@ -13,22 +13,23 @@ public class FizzBuzz {
         }
     }
 
-    public String runFizzBuzz(int inputNumber) {
+    public String runFizzBuzz(int inputNumber, Boolean rule3, Boolean rule5, Boolean rule7, Boolean rule11,
+                              Boolean rule13, Boolean rule17) {
         String finalOutput = "";
         ArrayList<String> wordList = new ArrayList<String>();
 
         if (inputNumber % 11 == 0) {
-            wordList.add(this.assessNumber(inputNumber, 13, "Fezz"));
-            wordList.add(this.assessNumber(inputNumber, 11, "Bong"));
+            wordList.add(this.assessNumber(inputNumber, 13, "Fezz", rule13));
+            wordList.add(this.assessNumber(inputNumber, 11, "Bong", rule11));
         }
         else {
-            wordList.add(this.assessNumber(inputNumber, 3, "Fizz"));
-            wordList.add(this.assessNumber(inputNumber, 13, "Fezz"));
-            wordList.add(this.assessNumber(inputNumber, 5, "Buzz"));
-            wordList.add(this.assessNumber(inputNumber, 7, "Bang"));
+            wordList.add(this.assessNumber(inputNumber, 3, "Fizz", rule3));
+            wordList.add(this.assessNumber(inputNumber, 13, "Fezz", rule13));
+            wordList.add(this.assessNumber(inputNumber, 5, "Buzz", rule5));
+            wordList.add(this.assessNumber(inputNumber, 7, "Bang", rule7));
         }
 
-        if (inputNumber % 17 == 0) {
+        if (inputNumber % 17 == 0 && rule17 == true) {
             Collections.reverse(wordList);
         }
 
--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -5,129 +5,135 @@ import static org.junit.Assert.assertEquals;
 
 public class FizzBuzzTest {
     @Test
-    public void assessNumberReturnsNumber() {
+    public void assessNumberReturnsEmptyString() {
         FizzBuzz fb = new FizzBuzz();
-        assertEquals("", fb.assessNumber(1, 3, "Fizz"));
+        assertEquals("", fb.assessNumber(1, 3, "Fizz", true));
     }
 
     @Test
     public void assessNumberReturnsFizz() {
         FizzBuzz fb = new FizzBuzz();
-        assertEquals("Fizz", fb.assessNumber(3, 3, "Fizz"));
+        assertEquals("Fizz", fb.assessNumber(3, 3, "Fizz", true));
     }
 
     @Test
     public void assessNumberReturnsBuzz() {
         FizzBuzz fb = new FizzBuzz();
-        assertEquals("Buzz", fb.assessNumber(5, 5, "Buzz"));
+        assertEquals("Buzz", fb.assessNumber(5, 5, "Buzz", true));
     }
 
     @Test
     public void assessNumberReturnsBang() {
         FizzBuzz fb = new FizzBuzz();
-        assertEquals("Bang", fb.assessNumber(7, 7, "Bang"));
+        assertEquals("Bang", fb.assessNumber(7, 7, "Bang", true));
+    }
+
+    @Test
+    public void assessNumberReturnsEmptyStringWhenInactive() {
+        FizzBuzz fb = new FizzBuzz();
+        assertEquals("", fb.assessNumber(3, 3, "Fizz", false));
     }
 
     @Test
     public void runFizzBuzz1() {
         FizzBuzz fb = new FizzBuzz();
-        assertEquals("1", fb.runFizzBuzz(1));
+        assertEquals("1", fb.runFizzBuzz(1, true, true, true, true, true, true));
     }
 
     @Test
     public void runFizzBuzz3() {
         FizzBuzz fb = new FizzBuzz();
-        assertEquals("Fizz", fb.runFizzBuzz(3));
+        assertEquals("Fizz", fb.runFizzBuzz(3, true, true, true, true, true, true));
     }
 
     @Test
     public void runFizzBuzz5() {
         FizzBuzz fb = new FizzBuzz();
-        assertEquals("Buzz", fb.runFizzBuzz(5));
+        assertEquals("Buzz", fb.runFizzBuzz(5, true, true, true, true, true, true));
     }
 
     @Test
     public void runFizzBuzz15() {
         FizzBuzz fb = new FizzBuzz();
-        assertEquals("FizzBuzz", fb.runFizzBuzz(15));
+        assertEquals("FizzBuzz", fb.runFizzBuzz(15, true, true, true, true, true, true));
     }
 
     @Test
     public void runFizzBuzz7() {
         FizzBuzz fb = new FizzBuzz();
-        assertEquals("Bang", fb.runFizzBuzz(7));
+        assertEquals("Bang", fb.runFizzBuzz(7, true, true, true, true, true, true));
     }
 
     @Test
     public void runFizzBuzz21() {
         FizzBuzz fb = new FizzBuzz();
-        assertEquals("FizzBang", fb.runFizzBuzz(21));
+        assertEquals("FizzBang", fb.runFizzBuzz(21, true, true, true, true, true, true));
     }
 
     @Test
     public void runFizzBuzz35() {
         FizzBuzz fb = new FizzBuzz();
-        assertEquals("BuzzBang", fb.runFizzBuzz(35));
+        assertEquals("BuzzBang", fb.runFizzBuzz(35, true, true, true, true, true, true));
     }
 
     @Test
     public void runFizzBuzz105() {
         FizzBuzz fb = new FizzBuzz();
-        assertEquals("FizzBuzzBang", fb.runFizzBuzz(105));
+        assertEquals("FizzBuzzBang", fb.runFizzBuzz(105, true, true, true, true, true, true));
     }
 
     @Test
     public void runFizzBuzz11() {
         FizzBuzz fb = new FizzBuzz();
-        assertEquals("Bong", fb.runFizzBuzz(11));
+        assertEquals("Bong", fb.runFizzBuzz(11, true, true, true, true, true, true));
     }
 
     @Test
     public void runFizzBuzz55() {
         FizzBuzz fb = new FizzBuzz();
-        assertEquals("Bong", fb.runFizzBuzz(55));
+        assertEquals("Bong", fb.runFizzBuzz(55, true, true, true, true, true, true));
     }
 
     @Test
     public void runFizzBuzz13() {
         FizzBuzz fb = new FizzBuzz();
-        assertEquals("Fezz", fb.runFizzBuzz(13));
+        assertEquals("Fezz", fb.runFizzBuzz(13, true, true, true, true, true, true));
     }
 
     @Test
     public void runFizzBuzz39() {
         FizzBuzz fb = new FizzBuzz();
-        assertEquals("FizzFezz", fb.runFizzBuzz(39));
+        assertEquals("FizzFezz", fb.runFizzBuzz(39, true, true, true, true, true, true));
     }
 
     @Test
     public void runFizzBuzz65() {
         FizzBuzz fb = new FizzBuzz();
-        assertEquals("FezzBuzz", fb.runFizzBuzz(65));
+        assertEquals("FezzBuzz", fb.runFizzBuzz(65, true, true, true, true, true, true));
     }
 
     @Test
     public void runFizzBuzz195() {
         FizzBuzz fb = new FizzBuzz();
-        assertEquals("FizzFezzBuzz", fb.runFizzBuzz(195));
+        assertEquals("FizzFezzBuzz", fb.runFizzBuzz(195, true, true, true, true, true, true));
     }
 
     @Test
     public void runFizzBuzz143() {
         FizzBuzz fb = new FizzBuzz();
-        assertEquals("FezzBong", fb.runFizzBuzz(143));
+        assertEquals("FezzBong", fb.runFizzBuzz(143, true, true, true, true, true, true));
     }
 
     @Test
     public void runFizzBuzz255() {
         FizzBuzz fb = new FizzBuzz();
-        assertEquals("BuzzFizz", fb.runFizzBuzz(255));
+        assertEquals("BuzzFizz", fb.runFizzBuzz(255, true, true, true, true, true, true));
     }
 
     @Test
     public void runFizzBuzz2431() {
         FizzBuzz fb = new FizzBuzz();
-        assertEquals("BongFezz", fb.runFizzBuzz(2431));
+        assertEquals("BongFezz", fb.runFizzBuzz(2431, true, true, true, true, true, true));
     }
 
 }
