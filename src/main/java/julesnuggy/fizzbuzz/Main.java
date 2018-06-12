5ec82d4ff62a7bcddc90e6d230d7a1d1282f50d2
Julian Ng <julian.ng@softwire.com>
2018-06-12 14:22:24 +0100

    Updated Main#main() to run to 200

--- src/main/java/julesnuggy/fizzbuzz/Main.java
+++ src/main/java/julesnuggy/fizzbuzz/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         FizzBuzz fb = new FizzBuzz();
-        for (int i=1; i<=105; i++) {
+        for (int i=1; i<=200; i++) {
             System.out.println(fb.runFizzBuzz(i));
         }
     }
